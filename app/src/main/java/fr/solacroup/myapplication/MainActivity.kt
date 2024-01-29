9fecb3c09442797d0eb12c4a0870be66c2b6130d
TetianaKfr <kosorukova.tetiana.a@gmail.com>
2024-01-29 11:24:36 +0100

    Test

---
+++ app/src/main/java/fr/solacroup/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.solacroup.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
